ca68d19f0bcc77028d1aba6046fb393f7308c4f0
Kajal Yadav <23051595@kiit.ac.in>
2026-01-30 13:26:23 +0530

    Initial React frontend

---
+++ App.js
@@ -0,0 +1,5 @@
+<Route path="/" element={<Home />} />
+<Route path="/login" element={<Login />} />
+<Route path="/product/:id" element={<Product />} />
+<Route path="/cart" element={<cart />} />
+<Route path="/thankyou" element={<thankyou />} />
